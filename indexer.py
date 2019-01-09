27b1ae2867a65f274aef933e631c72b629d99a70
violet <v.galushko@just-ai.com>
2019-01-09 23:14:49 +0300

    setdefault and file names added

--- indexer.py
+++ indexer.py
@@ -3,6 +3,7 @@ Indexer module is used for creating a database of words and numbers with their s
 """
 import shelve
 import fullTok
+from os import path
 
 
 class Database(object):
@@ -17,28 +18,21 @@ class Database(object):
         :param fileTxt: name of an input text file
         """
         # database is opened for operations
-        db = shelve.open(fileDB)
+        db = shelve.open(fileDB, writeback=True)
         # input file is opened for reading
         myFile = open(fileTxt, "r", encoding="UTF-8")
         toTok = myFile.read()
         myFile.close()
         # input file is tokenized
         data = fullTok.Tokenizer().tokenizeCat(toTok)
-        # list of token's positions
-        l = list()
 
         # checks type of each token
         for token in data:
             # if token is alphabetical or numerical, puts it into the database
             if(token.cat == "digit" or token.cat == "word"):
-                key = token.tok
-                # if same token is already in the database, adds new position to the list of its positions
-                if key not in db:
-                    l.clear()
-                else:
-                    l = db[key]
-                l.append(token.pos)
-                db[key] = l
+                # if same token is already in the database, adds new position
+                db.setdefault(token.tok, {})
+                db[token.tok].setdefault(path.basename(fileTxt), []).append(token.pos)
 
         print(dict(db))
         # database is closed
--- indexer_test.py
+++ indexer_test.py
@@ -16,39 +16,55 @@ class TestDatabase(unittest.TestCase):
         # Remove the directory after the test
         shutil.rmtree(self.temp_directory)
 
-    def create_textFile(self, directory, text):
+    def create_textFile(self, directory, name, text):
         # Create a file in the temporary directory and write something to it
-        f = open(path.join(directory, 'test.txt'), 'w', encoding='utf-8')
+        f = open(path.join(directory, name), 'w', encoding='utf-8')
         f.write(text)
         f.close()
         
     def test_simple_sentence(self):
-        TestDatabase().create_textFile(self.temp_directory, 'The owls are not what they seem.')
+        TestDatabase().create_textFile(self.temp_directory, 'test.txt', 'The owls are not what they seem.')
         indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'test.txt'))
         result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
         self.assertEqual(len(result), 7)
-        test_dict = {'the': [0], 'owls': [4], 'are': [9], 'not': [13],'what': [17], 'they': [22], 'seem': [27]}
+        test_dict = {'the': {'test.txt':[0]}, 'owls': {'test.txt':[4]}, 'are': {'test.txt':[9]}, 'not': {'test.txt':[13]},
+                     'what': {'test.txt':[17]}, 'they': {'test.txt':[22]}, 'seem': {'test.txt':[27]}}
         self.assertEqual(result, test_dict)
         result.close()
 
     def test_multicategory_string(self):
-        TestDatabase().create_textFile(self.temp_directory, '-Мама, помой раму! - сказала дочь 8, мама 589 64')
+        TestDatabase().create_textFile(self.temp_directory, 'test.txt', '-Мама, помой раму! - сказала дочь 8, мама 589 64')
         indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'test.txt'))
         result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
         self.assertEqual(len(result), 8)
-        test_dict = {'мама': [1, 37], 'помой': [7], 'раму': [13], 'сказала': [21], 'дочь': [29], '8': [34], '589': [42], '64': [46]}
+        test_dict = {'мама': {'test.txt':[1, 37]}, 'помой': {'test.txt':[7]}, 'раму': {'test.txt':[13]}, 'сказала': {'test.txt':[21]},
+                     'дочь': {'test.txt':[29]}, '8': {'test.txt':[34]}, '589': {'test.txt':[42]}, '64': {'test.txt':[46]}}
+        self.assertEqual(result, test_dict)
+        result.close()
+
+    def test_several_files(self):
+        TestDatabase().create_textFile(self.temp_directory, 'one.txt', '-Мама, помой раму! - сказала дочь.')
+        indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'one.txt'))
+        result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
+        self.assertEqual(len(result), 5)
+        TestDatabase().create_textFile(self.temp_directory, 'two.txt', 'Мама, ты помыла раму?')
+        indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'two.txt'))
+        result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
+        self.assertEqual(len(result), 7)
+        test_dict = {'мама': {'one.txt': [1], 'two.txt': [0]}, 'помой': {'one.txt': [7]}, 'раму': {'one.txt': [13], 'two.txt': [16]},
+                     'сказала': {'one.txt': [21]}, 'дочь': {'one.txt': [29]}, 'ты': {'two.txt': [6]}, 'помыла': {'two.txt': [9]}}
         self.assertEqual(result, test_dict)
         result.close()
 
     def test_punctuation_string(self):
-        TestDatabase().create_textFile(self.temp_directory, '- , ! ...')
+        TestDatabase().create_textFile(self.temp_directory, 'test.txt', '- , ! ...')
         indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'test.txt'))
         result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
         self.assertEqual(len(result), 0)
         result.close()
 
     def test_empty_string(self):
-        TestDatabase().create_textFile(self.temp_directory, '')
+        TestDatabase().create_textFile(self.temp_directory, 'test.txt', '')
         indexer.Database().toDB(path.join(self.temp_directory, 'testDB'), path.join(self.temp_directory, 'test.txt'))
         result = shelve.open(path.join(self.temp_directory, 'testDB'), 'r')
         self.assertEqual(len(result), 0)
